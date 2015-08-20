aa3a7c14664e6281d679f078c0646f39f5bcf4ed
Laurie Athey <laurie.athey@tombola.co.uk>
2015-08-20 13:40:18 +0100

    Love calculator done.

---
+++ js/functions-and-styling/1-love-calculator.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var firstName = 'Dan', secondName = 'Kane';
+var calcLove = function(name1, name2){
+
+    var nameLength = name1.length+name2.length-(Math.round(Math.random()*7));
+
+    var result = nameLength*42;
+
+    if(result >100) {
+        result = 100;
+    }
+    return result;
+};
+
+console.log(firstName + ' and ' + secondName + ' are ' + calcLove(firstName, secondName) + '% compatible.');
+console.log(firstName + ' and ' + secondName + ' are ' + calcLove(firstName, secondName) + '% compatible.');
+console.log(firstName + ' and ' + secondName + ' are ' + calcLove(firstName, secondName) + '% compatible.');
+console.log(firstName + ' and ' + secondName + ' are ' + calcLove(firstName, secondName) + '% compatible.');
+console.log(firstName + ' and ' + secondName + ' are ' + calcLove(firstName, secondName) + '% compatible.');
+console.log(firstName + ' and ' + secondName + ' are ' + calcLove(firstName, secondName) + '% compatible.');
+console.log(firstName + ' and ' + secondName + ' are ' + calcLove(firstName, secondName) + '% compatible.');
+console.log(firstName + ' and ' + secondName + ' are ' + calcLove(firstName, secondName) + '% compatible.');
+console.log(firstName + ' and ' + secondName + ' are ' + calcLove(firstName, secondName) + '% compatible.');
+console.log(firstName + ' and ' + secondName + ' are ' + calcLove(firstName, secondName) + '% compatible.');
