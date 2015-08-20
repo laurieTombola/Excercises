19b627029b5549862d5c87abb32494fa87ca9b4a
Laurie Athey <laurie.athey@tombola.co.uk>
2015-08-20 12:02:15 +0100

    The rest of the work for the numbers exercises.

---
+++ js/Numbers/4-D6.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var rollDice = function(sides) {
+    return Math.round(Math.random()*sides)+1;
+}
+
+console.log(rollDice(6));
+console.log(rollDice(6));
+console.log(rollDice(6));
+console.log(rollDice(6));
+console.log(rollDice(6));
+console.log(rollDice(6));
+console.log(rollDice(6));
+console.log(rollDice(6));
+console.log(rollDice(6));
+console.log(rollDice(6));
+console.log(rollDice(6));
+console.log(rollDice(6));
+console.log(rollDice(6));
+console.log(rollDice(6));
