9e50058604dfa1dcfe3afd5ed6fe9918d48828a4
Laurie Athey <laurie.athey@tombola.co.uk>
2015-08-21 11:04:26 +0100

    switching branches

---
+++ js/control-flow-and-errors/0-output-equilateral-triangle.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function rightAngle(size){
+    var stars = '';
+    for(var i = 0; i < size; i++){
+        stars = '';
+        stars += '*';
+        console.log(stars);
+    }
+}
+rightAngle(15);
