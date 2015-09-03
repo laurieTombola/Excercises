9fddf72a75c6ae398eff8385bb3f1228acdd882f
Laurie Athey <laurie.athey@tombola.co.uk>
2015-09-03 10:29:53 +0100

    Simplified the function down to 1 line.

--- js/arrays/2-array-to-CSV.js
+++ js/arrays/2-array-to-CSV.js
@@ -3,15 +3,7 @@
 var myArray = [1,2,3,4,5,6,7,8,9,0];
 
 function toCSV(array) {
-    var csvValue = '';
-    for(var i = 0; i < array.length; i++) {
-        csvValue += array[i].toString();
-        console.log(csvValue);
-        if(i+1 !== array.length) {
-            csvValue += ',';
-        }
-    }
-    return csvValue;
+    return array.join(',');
 }
 
 console.log(toCSV(myArray));
