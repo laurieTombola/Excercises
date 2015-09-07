f9b692d0454b6c37dfe06958f46ca648d8bfc7be
Laurie Athey <laurie.athey@tombola.co.uk>
2015-09-07 14:22:18 +0100

    Cleaning up and experimentation.

--- js/Objects/2-double-linked-lists.js
+++ js/Objects/2-double-linked-lists.js
@@ -1,71 +1,4 @@
 'use strict';
-//
-//function LinkedList(firstNode){
-//    var me = this;
-//    me.start = null;
-//    if (firstNode !== null) {
-//        me.start = firstNode;
-//    }
-//    me.insert = function(previousNode, nextNode, newNode){
-//        if(previousNode === null && nextNode !== null){
-//            //insert before nextNode
-//            previousNode = me.findPreviousNode(nextNode);
-//            if(previousNode !== null) {
-//                previousNode.next = newNode;
-//            }
-//            newNode.next = nextNode;
-//            if(nextNode === me.start){
-//                me.start = newNode;
-//            }
-//        }
-//        else if(previousNode !== null && nextNode === null){
-//            //insert after previousNode
-//            nextNode = previousNode.next;
-//            newNode.next = nextNode;
-//            previousNode.next = newNode;
-//        }
-//    };
-//
-//    me.remove = function(obj){
-//
-//        if(me.start === obj) {
-//            if( obj.next === null){
-//                me.start = null;
-//            }
-//            else {
-//                me.start = obj.next;
-//            }
-//        }
-//        else if(obj.next === null)
-//        {
-//            me.findPreviousNode(obj).next = null;
-//        }
-//        else {
-//            console.log("Laurie Was 'Er");
-//            me.findPreviousNode(obj).next = obj.next;
-//        }
-//    };
-//
-//    me.findPreviousNode = function(node) {
-//        var current = me.start;
-//        while(current.next!==node) {
-//            if(current.next === null) {
-//                break;
-//            }
-//            current = current.next;
-//        }
-//        if(current.next === node) {
-//            return current.next;
-//        }
-//        else {
-//            //console.log("couldn't find the node you pass into findPreviousNode. Returning Null.");
-//            return null;
-//        }
-//
-//    };
-//}
-
-
 
 function Node(nodeValue){
     var me = this;
@@ -161,3 +94,54 @@ console.log("The New list - 5");
 myNode.next.next.removeNext();
 
 outputList(myNode);
+
+
+/////////////////////////////////////////////////////////////
+                /*ATTEMPT NUMBER 3*/
+/////////////////////////////////////////////////////////////
+
+function Node(nodeValue){
+    var me = this;
+    me.value = nodeValue;
+    me.next = null;
+
+    me.removeNext = function(){
+        if(me.next === null){
+            return;
+        }
+        else if( me.next.next === null){
+            me.next = null;
+        }
+        else {
+            me.next = me.next.next;
+        }
+    };
+
+    me.insert = function(newNode){
+        if(me.next !== null){
+            var nextNext = me.next;
+            me.next = newNode;
+            newNode.next = nextNext;
+        }
+        else {
+            me.next = newNode;
+        }
+    };
+}
+
+var DoubleLinkedNode = new Node(null);
+
+
+DoubleLinkedNode.insertAfter = function(newNode){
+    DoubleLinkedNode.__proto__.insert(newNode);
+
+    if(me.next !== null){
+        newNode.next = me.next;
+        newNode.previous = me;
+        newNode.next.previous = newNode;
+    }
+    else {
+        me.next = newNode;
+        me.next.previous = me;
+    }
+}
